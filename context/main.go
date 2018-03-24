1fb89fb4c92cc79f0e8216f45b6d01beb268e34f
kamakuni <superradio86@gmail.com>
2018-03-24 23:33:47 +0900

    add turorial for the context package

---
+++ context/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	queue := make(chan string)
+	for i := 0; i < 2; i++ {
+		wg.Add(1)
+		go fetchURL(ctx, queue)
+	}
+
+	queue <- "http://www.example.com"
+	queue <- "http://www.example.net"
+	queue <- "http://www.example.net/foo"
+	queue <- "http://www.example.net/bar"
+
+	cancel()
+	wg.Wait()
+}
+
+func fetchURL(ctx context.Context, queue chan string) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("worker exit")
+			wg.Done()
+			return
+		case url := <-queue:
+			fmt.Println("fetching ", url)
+		}
+	}
+}
