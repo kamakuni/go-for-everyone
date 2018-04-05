866a4092afb85a4ec0daed2d7e276131fe12e4af
kamakuni <superradio86@gmail.com>
2018-04-05 23:21:38 +0900

    add codes to print tags

--- parse-struct/main.go
+++ parse-struct/main.go
@@ -1,10 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type Point struct {
 	X int `urlenc:"x,omitempty"`
-	Y int `irlenc:"y,omitempty"`
+	Y int `urlenc:"y,omitempty"`
 }
 
 func main() {
-
+	t := reflect.TypeOf(Point{X: 1, Y: 2})
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		if f.PkgPath != "" {
+			continue
+		}
+		fmt.Println(f.Tag.Get("urlenc"))
+	}
 }
