ce292fa6701b01c16e0c2e989b59112512efbba7
kamakuni <superradio86@gmail.com>
2018-04-29 23:31:23 +0900

    add benchmarking for reflections

---
+++ reflect-performance/main_test.go
@@ -0,0 +1,27 @@
+package reflect
+
+import (
+	"reflect"
+	"testing"
+)
+
+type StructAccess struct {
+	Int int
+}
+
+func BenchmarkDetectTypeReflect(b *testing.B) {
+	var s interface{} = StructAccess{Int: 100}
+	for i := 0; i < b.N; i++ {
+		rv := reflect.ValueOf(s)
+		if rv.Type().Name() == "StructAccess" {
+			_ = s.(StructAccess).Int
+		}
+	}
+}
+
+func BenchmarkDetectNone(b *testing.B) {
+	s := StructAccess{Int: 100}
+	for i := 0; i < b.N; i++ {
+		_ = s.Int
+	}
+}
