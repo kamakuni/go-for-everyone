371adfc1d9062b5588b3de49d89c474166c09c6e
kamakuni <superradio86@gmail.com>
2018-04-30 23:53:01 +0900

    create codes for sort with reflection

---
+++ reflect-sort/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"reflect"
+)
+
+type Sortwrap struct {
+	value    reflect.Value
+	lessfunc func(int, int) bool
+}
+
+func NewSortwrap(v interface{}, lessfunc func(int, int) bool) *Sortwrap {
+	return &Sortwrap{
+		value:    reflect.ValueOf(v),
+		lessfunc: lessfunc,
+	}
+}
+
+func main() {
+
+}
